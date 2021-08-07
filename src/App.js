3456047950a8129a6f091a4c0f3bbc11e4cd61eb
dmitriysemenko <dm123semenko@gmail.com>
2021-08-07 21:20:14 -0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Player from "./components/Player";
+import Song from "./components/Song";
+
 function App() {
-  return (
-    <div className="App">
-        <h1>Music Player</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Song />
+            <Player />
+        </div>
+    );
 }
 
 export default App;
