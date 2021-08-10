8cbc3a4ffca1387965c1ebdca2e3d8af34c2c247
dmitriysemenko <dm123semenko@gmail.com>
2021-08-10 22:10:30 -0100

    update

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ function App() {
     // State
     const [songs, setSongs] = useState(data());
     const [currentSong, setCurrentSong] = useState(songs[0]);
+    const [isPlaying, setIsPlaying] = useState(false);
     return (
         <div className="App">
             <Song currentSong={currentSong} />
-            <Player currentSong={currentSong} />
+            <Player
+                currentSong={currentSong}
+                isPlaying={isPlaying}
+                setIsPlaying={setIsPlaying}
+            />
         </div>
     );
 }
