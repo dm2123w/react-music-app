c9c0b7f855a2d0f905db2b5fab4342ab8de568eb
dmitriysemenko <dm123semenko@gmail.com>
2021-08-12 21:31:48 -0100

    update

--- src/components/Player.js
+++ src/components/Player.js
@@ -35,6 +35,11 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
         );
     };
 
+    const dragHandler = (e) => {
+        audioRef.current.currentTime = e.target.value;
+        setSongInfo({ ...songInfo, currentTime: e.target.value });
+    };
+
     // State
     const [songInfo, setSongInfo] = useState({
         currentTime: null,
@@ -45,7 +50,13 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
         <div className="player">
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
-                <input type="range" />
+                <input
+                    min={0}
+                    max={songInfo.duration}
+                    value={songInfo.currentTime}
+                    onChange={dragHandler}
+                    type="range"
+                />
                 <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
