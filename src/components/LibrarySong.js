a68b2b193cdaf4c13dc8f83e222cf2bbb8ddff25
dmitriysemenko <dm123semenko@gmail.com>
2021-08-13 23:39:55 +0500

    update

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const LibrarySong = ({ song }) => {
     return (
-        <div className="library-song">
+        <div className="library-songs">
             <img alt={song.name} src={song.cover}></img>
-            <h3>{song.name}</h3>
-            <h4>{song.artist}</h4>
+            <div className="song-description">
+                <h3>{song.name}</h3>
+                <h4>{song.artist}</h4>
+            </div>
         </div>
     );
 };
