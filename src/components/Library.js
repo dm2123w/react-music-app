2906093d2a8b124c7c89723830071d80d89a8c98
dmitriysemenko <dm123semenko@gmail.com>
2021-08-13 22:30:38 +0500

    update

---
+++ src/components/Library.js
@@ -0,0 +1,11 @@
+import React from "react";
+import LibrarySong from "./LibrarySong";
+
+const Library = () => {
+    return <div className="library">
+        <h2>Library</h2>
+        <div className="library-songs">
+            <LibrarySong/>
+        </div>
+    </div>
+};
