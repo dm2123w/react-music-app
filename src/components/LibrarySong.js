f8ab8605131050dd14bd4d7187dc17d8a6f31b9d
dmitriysemenko <dm123semenko@gmail.com>
2021-08-20 17:22:26 +0300

    update

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { playAudio } from "../util";
 
 const LibrarySong = ({
     song,
@@ -10,8 +9,8 @@ const LibrarySong = ({
     isPlaying,
     setSongs,
 }) => {
-    const songSelectHandler = () => {
-        setCurrentSong(song);
+    const songSelectHandler = async () => {
+        await setCurrentSong(song);
 
         // Add active state
         const newSongs = songs.map((song) => {
@@ -30,7 +29,7 @@ const LibrarySong = ({
         setSongs(newSongs);
 
         // Check if the song is playing
-        playAudio(isPlaying, audioRef);
+        if (isPlaying) audioRef.current.play();
     };
     return (
         <div
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { playAudio } from "../util";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faPlay,
@@ -61,22 +60,22 @@ const Player = ({
         setSongInfo({ ...songInfo, currentTime: e.target.value });
     };
 
-    const skipTrackHandler = (direction) => {
+    const skipTrackHandler = async (direction) => {
         let currentIndex = songs.findIndex(
             (song) => song.id === currentSong.id
         );
         if (direction === "skip-forward") {
-            setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+            await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
         }
         if (direction === "skip-back") {
             if ((currentIndex - 1) % songs.length === -1) {
-                setCurrentSong(songs[songs.length - 1]);
-                playAudio(isPlaying, audioRef);
+                await setCurrentSong(songs[songs.length - 1]);
+                if (isPlaying) audioRef.current.play();
                 return;
             }
-            setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+            await setCurrentSong(songs[(currentIndex - 1) % songs.length]);
         }
-        playAudio(isPlaying, audioRef);
+        if (isPlaying) audioRef.current.play();
     };
 
     // Add the styles
@@ -88,7 +87,12 @@ const Player = ({
         <div className="player">
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
-                <div className="track" style={{background: `linear-gradient(to right, ${currentSong.color[0]}, ${currentSong.color[1]})`}}>
+                <div
+                    className="track"
+                    style={{
+                        background: `linear-gradient(to right, ${currentSong.color[0]}, ${currentSong.color[1]})`,
+                    }}
+                >
                     <input
                         min={0}
                         max={songInfo.duration || 0}
